19d215b44b1c47eea35be1990473606689fbfbf7
JK <jkdesign@me.com>
2016-02-25 15:03:38 +0100

    work release

--- ScriptumX/app/urls.py
+++ ScriptumX/app/urls.py
@@ -24,4 +24,14 @@ urlpatterns = patterns('',
     url(r'^(?P<poll_id>\d+)/vote/$', 'app.views.vote', name='vote'),
 
     url(r'^gadget/(?P<gadget_id>\d+)?$', 'app.views.gadget', name='gadget'),
+
+    url(r'^project/(?P<id>\d+)?$', 'app.views.dummy', name='dummy'),
+    url(r'^script/(?P<id>\d+)?$', 'app.views.dummy', name='dummy'),
+    url(r'^scene/(?P<id>\d+)?$', 'app.views.dummy', name='dummy'),
+    url(r'^set/(?P<id>\d+)?$', 'app.views.dummy', name='dummy'),
+    url(r'^role/(?P<id>\d+)?$', 'app.views.dummy', name='dummy'),
+    url(r'^folk/(?P<id>\d+)?$', 'app.views.dummy', name='dummy'),
+    url(r'^sfx/(?P<id>\d+)?$', 'app.views.dummy', name='dummy'),
+    url(r'^audio/(?P<id>\d+)?$', 'app.views.dummy', name='dummy'),
+    url(r'^schedule/(?P<id>\d+)?$', 'app.views.dummy', name='dummy'),
 )
--- ScriptumX/app/views.py
+++ ScriptumX/app/views.py
@@ -151,6 +151,37 @@ def seed(request):
 #        return classmodel.objects.get(**kwargs)
 #    except classmodel.DoesNotExist:
 #        return None
+tab_list1 = (
+    ( 'P', 'Project', '/gadget', 'app/img/Tab/Project-16.png' ),
+    ( 'G', 'Gadget',  '/gadget', 'app/img/Tab/Gadget-16.png' ),
+    )
+
+tab_list2 = (
+    { 'active':'P', 'name':'Project',  'href':'/project',  'img':'app/img/Tab/Project-16.png' },
+    { 'active':'C', 'name':'Script',   'href':'/script',   'img':'app/img/Tab/Script-16.png' },
+    { 'active':'S', 'name':'Scene',    'href':'/scene',    'img':'app/img/Tab/Scene-16.png' },
+    { 'active':'L', 'name':'Set',      'href':'/set',      'img':'app/img/Tab/Set-16.png' },
+    { 'active':'R', 'name':'Role',     'href':'/role',     'img':'app/img/Tab/Role-16.png' },
+    { 'active':'F', 'name':'Folk',     'href':'/folk',     'img':'app/img/Tab/Folk-16.png' },
+    { 'active':'G', 'name':'Gadget',   'href':'/gadget',   'img':'app/img/Tab/Gadget-16.png' },
+    { 'active':'X', 'name':'SFX',      'href':'/sfx',      'img':'app/img/Tab/SFX-16.png' },
+    { 'active':'A', 'name':'Audio',    'href':'/audio',    'img':'app/img/Tab/Audio-16.png' },
+    { 'active':'T', 'name':'Schedule', 'href':'/schedule', 'img':'app/img/Tab/Schedule-16.png' },
+    )
+
+gadget_tag_list = (
+    { 'bit': 0, 'name':'Requisite', 'img':'app/img/G/tag/Requisite.png' },
+    { 'bit': 1, 'name':'Costume',   'img':'app/img/G/tag/Costume.png' },
+    { 'bit': 2, 'name':'MakeUp',    'img':'app/img/G/tag/MakeUp.png' },
+    { 'bit': 3, 'name':'Camera',    'img':'app/img/G/tag/Camera.png' },
+    { 'bit': 4, 'name':'Gaffer',    'img':'app/img/G/tag/Gaffer.png' },
+    { 'bit': 5, 'name':'Grip',      'img':'app/img/G/tag/Grip.png' },
+    { 'bit': 6, 'name':'Audio',     'img':'app/img/G/tag/Audio.png' },
+    { 'bit': 7, 'name':'Special',   'img':'app/img/G/tag/Special.png' },
+    { 'bit': 8, 'name':'Tool',      'img':'app/img/G/tag/Tool.png' },
+    { 'bit': 9, 'name':'Phyro',     'img':'app/img/G/tag/Phyro.png' },
+    { 'bit':10, 'name':'Catering',  'img':'app/img/G/tag/Catering.png' },
+    )
 
 def gadget(request, gadget_id):
     """Handles ..."""
@@ -165,12 +196,27 @@ def gadget(request, gadget_id):
         active_id = None
 
     return render(request, 'app/gadget.html', {
-        'title': 'Gadgets',
+        'title': 'Gadget',
+        'tab_active': 'G',
+        'tab_list': tab_list1,
+        'tab_list2': tab_list2,
+        'tag_list': gadget_tag_list,
         'datetime': datetime.now(),
         'gadgets': gadgets,
         'active_gadget': active_gadget,
         'active_id': active_id,
         #'error_message': "Please make a selection.",
+    })
+
+def dummy(request, id):
+    """Handles ..."""
+    
+    return render(request, 'app/gadget.html', {
+        'title': 'DUMMY',
+        'tab_active': 'P',
+        'tab_list': tab_list1,
+        'tab_list2': tab_list2,
+        'datetime': datetime.now(),
     })
 
 
