9bfb8194130c45a08ce1e41176af4fb2aac9a1b3
JK <jkdesign@me.com>
2016-04-19 15:04:49 +0200

    work release

--- ScriptumX/ScriptumX/settings.py
+++ ScriptumX/ScriptumX/settings.py
@@ -20,6 +20,17 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': path.join(PROJECT_ROOT, 'db.sqlite3'),
+#        'USER': '',
+#        'PASSWORD': '',
+#        'HOST': '',
+#        'PORT': '',
+#    }
+#}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
