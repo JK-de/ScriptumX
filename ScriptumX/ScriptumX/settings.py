27d05b2a4e72da6e0a4527d09e223bfb25429168
JK <jkdesign@me.com>
2016-05-03 20:32:55 +0200

    moved img-folder to ScriptumX

--- ScriptumX/ScriptumX/settings.py
+++ ScriptumX/ScriptumX/settings.py
@@ -161,6 +161,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     #my apps
+    'ScriptumX',
     'X',
     'web',
     'authentication',
